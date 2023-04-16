44ba07c40fcab1816b19ad6c4983d97a0614e584
enderson <enderson.linhares20@gmail.com>
2023-04-16 20:12:45 -0300

    fix css

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -6,13 +6,13 @@ export const LayoutContainer = styled.div`
   height: calc(100vh);
   padding: 6.4rem;
   min-width: 100vw;
-  background: url(${back}) right top / contain no-repeat ;  
+  background: url(${back}) right bottom / contain no-repeat ;  
 
   display: flex;
   flex-direction: column;
   gap: 10rem;
 
-  @media(max-width: 840px){
+  @media(max-width: 970px){
     background: none;
     align-items: center;
   }
